2f061f3add0fa60dca8eb542b773ef9ea2e8a5b8
dsasmita <dangs.work@gmail.com>
2020-01-23 20:23:27 +0700

    create form habit component

---
+++ src/components/FormHabit.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+class FormHabit extends React.Component {
+  constructor() {
+    super();
+    this.state = { habit: "", status: "Mulai" };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const newHabit = {
+      habit: this.state.habit,
+      status: this.state.status
+    };
+    this.setState({
+      habit: "",
+      status: "Mulai"
+    });
+    console.log("Sibmit data ...", [newHabit]);
+  };
+
+  handleChangeHabit = event => {
+    this.setState({ habit: event.target.value });
+  };
+
+  handleStatus = event => {
+    this.setState({ status: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="form-input">
+        <form onSubmit={this.handleSubmit}>
+          <select
+            name="status"
+            onChange={this.handleStatus}
+            value={this.state.status ? this.state.status : "Mulai"}
+            ref="status"
+          >
+            <option value="Mulai">Mulai</option>
+            <option value="Berhenti">Berhenti</option>
+          </select>
+          <input
+            type="text"
+            name="habit"
+            onChange={this.handleChangeHabit}
+            value={this.state.habit}
+            placeholder="habit..."
+          />
+          <button type="submit">Simpan</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default FormHabit;
